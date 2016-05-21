c99b2c41f63b052be9385e3f25923566417832ab
Marco Brigham <marco@brigham.be>
2016-05-21 23:52:21 +0100

    Renamed tools package to utils. Added random forest classifier, with n-cross-validation.

--- quality_detector.py
+++ quality_detector.py
@@ -3,14 +3,15 @@
 
 import numpy as np
 import os
-from tools import easy_import
-
+from utils import easy_import
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+import multiprocessing
 
 class Config:
     """Configuration settings."""
     def __init__(self):
         self.data_root = None
-
 
 def predict_quality(record):
     """Predict the quality of the signal.
@@ -37,6 +38,25 @@ if __name__ == "__main__":
 
     config = Config()
     config.data_root = '../datasets'
+    nprocs = multiprocessing.cpu_count()
 
-    record = os.path.join(config.data_root, "record1.h5")
-    results = predict_quality(record)
+    record = os.path.join(config.data_root, "quality_dataset.h5")
+
+    raws, filtered, labels = easy_import.extract_data(record)
+
+    for item in [10, 50, 100, 200, 300, 400, 500]:
+
+        print 'n_estimators=', item
+
+        rf = RandomForestClassifier(n_estimators=item, n_jobs=nprocs)
+
+        scores = cross_validation.cross_val_score(rf, raws, labels, cv=5)
+
+        print("Accuracy (raw): %0.2f (+/- %0.2f)" % (scores.mean() * 100, scores.std() * 100 * 2))
+
+        # scores = cross_validation.cross_val_score(rf, filtered, labels, cv=5)
+        #
+        # print("Accuracy (filt): %0.2f (+/- %0.2f)" % (scores.mean() * 100, scores.std() * 100 * 2))
+
+        # record = os.path.join(config.data_root, "record1.h5")
+        # results = predict_quality(record)
