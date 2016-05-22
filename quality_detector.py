b1b38a5378a4ed1424978d15abb9b3630130b1e4
Marco Brigham <marco@brigham.be>
2016-05-22 18:15:14 +0100

    Added main documentation, HDF5 dataset explorer and grid parameter search.

--- quality_detector.py
+++ quality_detector.py
@@ -3,15 +3,8 @@
 
 import numpy as np
 import os
-from utils import easy_import
-from sklearn import cross_validation
+from utils import Config, easy_import
 from sklearn.ensemble import RandomForestClassifier
-import multiprocessing
-
-class Config:
-    """Configuration settings."""
-    def __init__(self):
-        self.data_root = None
 
 def predict_quality(record):
     """Predict the quality of the signal.
@@ -26,10 +19,12 @@ def predict_quality(record):
     of the 4 channels of the record at each timestep.
     This results must have the same size as the channels.
     """
+
     # Extract signals from record
     raws, filtered = easy_import.extract_signals(record)
+
     # Initialize results (same size as the channels)
-    results = np.zeros((raws.shape))
+    results = np.zeros(raws.shape)
 
     return results
 
@@ -37,26 +32,17 @@ def predict_quality(record):
 if __name__ == "__main__":
 
     config = Config()
-    config.data_root = '../datasets'
-    nprocs = multiprocessing.cpu_count()
+    config.data_root = "../datasets"
 
     record = os.path.join(config.data_root, "quality_dataset.h5")
 
-    raws, filtered, labels = easy_import.extract_data(record)
+    # retrieve train dataset
+    raw, filtered, labels = easy_import.extract_signals_train(record)
 
-    for item in [10, 50, 100, 200, 300, 400, 500]:
+    # fit Random Forest
+    rf = RandomForestClassifier(n_jobs=-1)
+    rf.fit(raw, labels)
 
-        print 'n_estimators=', item
-
-        rf = RandomForestClassifier(n_estimators=item, n_jobs=nprocs)
-
-        scores = cross_validation.cross_val_score(rf, raws, labels, cv=5)
-
-        print("Accuracy (raw): %0.2f (+/- %0.2f)" % (scores.mean() * 100, scores.std() * 100 * 2))
-
-        # scores = cross_validation.cross_val_score(rf, filtered, labels, cv=5)
-        #
-        # print("Accuracy (filt): %0.2f (+/- %0.2f)" % (scores.mean() * 100, scores.std() * 100 * 2))
-
-        # record = os.path.join(config.data_root, "record1.h5")
-        # results = predict_quality(record)
+    # retrieve test dataset
+    record = os.path.join(config.data_root, "record1.h5")
+    results = predict_quality(record)
